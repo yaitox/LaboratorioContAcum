2e0063d1d52763ba7bc1d5cf05bf887173999b67
Pedro Javier Bohorquez Camelo <000405199@bga.upb>
2019-03-08 13:43:06 -0500

    ejercicio resuelto

--- primos.py
+++ primos.py
@@ -1,8 +1,23 @@
 
 contCero=0
-a=int(input("cuanto numeros quiere evaluar"))
+a=int(input("ingrese un numero"))
 for i in range(1,a):
     residuo=a%i
+    if residuo==0:
+        contCero=contCero+1
 
-    print(residuo)
+        if (contCero == 2):
+            print("no es primo")
+            break
+
+
+    else:
+        if(contCero==1):
+                print("es primo")
+                break
+
+
+
+
+
 
